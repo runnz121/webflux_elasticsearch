c3ea47c42e200e631f3d0a21c4a7791ef1ec3b6f
runnz121 <runnz121@gmail.com>
2025-03-20 21:51:18 +0900

    first commit

---
+++ src/test/java/kuku/webflus/es/EsApplicationTests.java
@@ -0,0 +1,13 @@
+package kuku.webflus.es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
